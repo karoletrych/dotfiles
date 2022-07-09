90affc0b82df9f5c788f3151d986cc44cef786a8
Karol Etrych <karol.etrych@gmail.com>
2022-07-09 16:55:43 +0200

    Update install.ps1

--- manual/powershell/install.ps1
+++ manual/powershell/install.ps1
@@ -1,6 +1,6 @@
 set-executionpolicy unrestricted
 
-winget install --id Microsoft.Powershell --source winget
+winget install --id Microsoft.Powershell.Preview --version "7.3.0.5" --source winget
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
