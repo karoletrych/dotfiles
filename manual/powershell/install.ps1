fd6d6519856bfdd7afc0c26873c74735b51adb1d
Karol Etrych <karol.etrych@gmail.com>
2022-07-09 17:41:53 +0200

    Update install.ps1

--- manual/powershell/install.ps1
+++ manual/powershell/install.ps1
@@ -2,6 +2,8 @@ set-executionpolicy unrestricted
 
 winget install --id Microsoft.Powershell.Preview --version "7.3.0.5" --source winget
 
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
 Install-Module Terminal-Icons -Repository PSGallery
